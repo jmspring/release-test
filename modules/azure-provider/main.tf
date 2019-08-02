ea3a228752bf9955b30fa647f9c6103cc4b0fcd4
jim spring <jmspring@gmail.com>
2019-08-02 02:47:32 +0000

    create branch for 0.1.4

--- modules/azure-provider/main.tf
+++ modules/azure-provider/main.tf
@@ -8,5 +8,5 @@ provider "azuread" {
 
 # common modules
 module "common-provider" {
-  source = "github.com/jmspring/release-test?ref=master//modules/common-provider"
+  source = "github.com/jmspring/release-test?ref=0.1.4//modules/common-provider
 }
--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,5 +1,5 @@
 module "provider" {
-  source = "github.com/jmspring/release-test?ref=master//modules/azure-provider"
+  source = "github.com/jmspring/release-test?ref=0.1.4//modules/azure-provider
 }
 
 data "azurerm_resource_group" "vnet" {
